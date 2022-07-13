74a655c23f586677ea8eb8ed0855f521a115f4c2
Aleksandra Makhrova <amakhro@gmail.com>
2022-07-12 17:27:51 -0700

    first commit

---
+++ PetSitter/Models/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  PetSitter
+//
+//  Created by antikiller on 12.07.2022.
+//
+
+import Foundation
---
+++ PetSitter/Views/MainPageView.swift
@@ -0,0 +1,20 @@
+//
+//  MainPageView.swift
+//  PetSitter
+//
+//  Created by antikiller on 12.07.2022.
+//
+
+import SwiftUI
+
+struct MainPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainPageView()
+    }
+}
