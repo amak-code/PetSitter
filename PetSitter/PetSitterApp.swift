c7bfa57204ea092b47820852bcefaff0edfcf7cb
Aleksandra Makhrova <amakhro@gmail.com>
2022-07-12 17:10:55 -0700

    Initial Commit

---
+++ PetSitter/PetSitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetSitterApp.swift
+//  PetSitter
+//
+//  Created by antikiller on 12.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct PetSitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
