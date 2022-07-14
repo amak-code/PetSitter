36da25995d26b44bc93d5107f9e47b3bf3995109
Aleksandra Makhrova <amakhro@gmail.com>
2022-07-14 08:50:38 -0700

    Map View was created

--- PetSitter/Views/MapView.swift
+++ PetSitter/Views/MapView.swift
@@ -6,10 +6,26 @@
 //
 
 import SwiftUI
+import CoreLocation
+import MapKit
 
 struct MapView: View {
+    
+    @StateObject private var viewModel = MapViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            Map(coordinateRegion: $viewModel.mapRegion, showsUserLocation: true, userTrackingMode: .constant(.follow))
+                .ignoresSafeArea()
+            Circle()
+                .fill(.indigo)
+                .opacity(0.6)
+                .frame(width: 32, height: 32)
+            .onAppear{
+                viewModel.checkLocationServicesIsEnabled()
+            }
+        }
     }
 }
 
@@ -17,4 +33,48 @@ struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
     }
+}
+
+final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager?
+    
+    @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37, longitude: -122), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta:0.5))
+    
+    func checkLocationServicesIsEnabled(){
+        
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager = CLLocationManager()
+            locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager?.delegate = self
+        }
+        else {
+            print("ALert")
+        }
+        
+    }
+    
+    func checkLoactionAuthorization(){
+        
+        guard let locationManager = locationManager else {return}
+        
+        switch locationManager.authorizationStatus {
+            
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("Parental control")
+        case .denied:
+            print("You have denied this app location permission.")
+        case .authorizedAlways, .authorizedWhenInUse:
+            mapRegion = MKCoordinateRegion(center: locationManager.location?.coordinate ?? CLLocationCoordinate2D(latitude: 37, longitude: -122), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta:0.5))
+        @unknown default:
+            break
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLoactionAuthorization()
+    }
+    
 }
